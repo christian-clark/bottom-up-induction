045c3c2ea0ff10575ea5bd597927ed9d891bbd34
Christian Clark <clark.3664@osu.edu>
2024-10-05 22:24:04 -0400

    count valid pred trees

--- count_pred_trees.py
+++ count_pred_trees.py
@@ -1,4 +1,4 @@
-import numpy as np, sys
+import math, numpy as np, sys
 
 nmax = int(sys.argv[1])
 
@@ -6,12 +6,45 @@ nmax = int(sys.argv[1])
 # row[0] is number of trees with x0 category
 # row[1] is number of trees with x1 category
 # row[2] is number of trees with x2 category
-c = np.zeros((nmax, 3))
+
+# the sum of row n of c gives the number of possible pred trees with n+1
+# leaves, with the leaves fixed in one particular order. Need to multiply
+# by (n+1)! to get the total
+c = np.zeros((nmax, 3), dtype=int)
 c[0, 2] = 1
 
 for n in range(1, nmax):
     x0_n = 0
-    for k in range(1, n-1):
-        x0_n += c[k, 0]*c[n-k, 0] # TODO continue here 
+    for k in range(0, n):
+        x0_n += c[k,0]*c[n-k-1,0] \
+        + 2*c[k,1]*c[n-k-1,0] \
+        + c[k,1]*c[n-k-1,1] \
+        + c[k,1]*c[n-k-1,2] \
+        + 2*c[k,2]*c[n-k-1,0] \
+        + c[k,2]*c[n-k-1,1] \
+        + c[k,2]*c[n-k-1,2]
+    c[n, 0] = x0_n
 
-print(c)
+    x1_n = 0
+    for k in range(0, n):
+        x1_n += c[k,0]*c[n-k-1,1] \
+        + c[k,1]*c[n-k-1,1] \
+        + c[k,2]*c[n-k-1,0] \
+        + 2*c[k,2]*c[n-k-1,1] \
+        + c[k,2]*c[n-k-1,2]
+    c[n, 1] = x1_n
+
+    x2_n = 0
+    for k in range(0, n):
+        x2_n += c[k,0]*c[n-k-1,2] \
+        + c[k,1]*c[n-k-1,2] \
+        + c[k,2]*c[n-k-1,2]
+    c[n, 2] = x2_n
+
+
+totals = c.sum(axis=1)
+for t in totals:
+    print(t)
+# this is the full total when all permutations of leaves are considered
+#for n, t in enumerate(totals):
+#    print(t * math.factorial(n+1))
